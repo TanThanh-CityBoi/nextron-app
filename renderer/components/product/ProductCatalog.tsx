5fcff2ceab9f316d225c0f134c8c2bd397165a77
TanThanh-CityBoi <tanthanhe@gmail.com>
2024-03-08 17:32:11 +0700

    .

--- renderer/components/product/ProductCatalog.tsx
+++ renderer/components/product/ProductCatalog.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "./ProductCard";
+import { useState } from "react";
 
 const ProductCatalog = () => {
    const products = [
@@ -49,30 +50,38 @@ const ProductCatalog = () => {
       },
    ];
 
+   const [collapsed, setCollapsed] = useState(false);
+
    return (
-      <div className="">
-         <div className="flex justify-between py-4">
-            <div className="bg-primary-300 rounded-e-full py-2 px-4">
-               <h5> Danh sách sản phẩm</h5>
+      <div className="flex">
+         <div className="w-full">
+            <div className="flex justify-between py-4">
+               <div className="bg-primary-300 rounded-e-full py-2 px-4">
+                  <h5> Danh sách sản phẩm</h5>
+               </div>
+               <div>
+                  <button onClick={() => setCollapsed(!collapsed)}>Collaped</button>
+               </div>
             </div>
-            <div></div>
+            <div className="grid gap-x-4 gap-y-6 grid-cols-[repeat(auto-fill,minmax(min(10rem,100%),1fr))] p-4 overflow-x-scroll small-scrollbar h-[50vh]">
+               {products.map((item, id) => {
+                  return (
+                     <div key={id}>
+                        <ProductCard
+                           thumbnail={item.thumbnail}
+                           price={item.price}
+                           name={item.name}
+                           sellPrice={item.sellPrice}
+                           amount={item.amount}
+                           discount={item.discount}
+                        ></ProductCard>
+                     </div>
+                  );
+               })}
+            </div>
          </div>
-         <div className="grid gap-x-4 gap-y-6 grid-cols-[repeat(auto-fill,minmax(min(10rem,100%),1fr))] p-4 overflow-x-scroll small-scrollbar h-[50vh]">
-            {products.map((item, id) => {
-               return (
-                  <div key={id}>
-                     <ProductCard
-                        thumbnail={item.thumbnail}
-                        price={item.price}
-                        name={item.name}
-                        sellPrice={item.sellPrice}
-                        amount={item.amount}
-                        discount={item.discount}
-                     ></ProductCard>
-                  </div>
-               );
-            })}
-         </div>
+
+         <div className={`bg-gray-400 ${collapsed ? "w-0" : "w-1/3"}`}></div>
       </div>
    );
 };

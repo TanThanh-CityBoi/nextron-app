b3f51829be027ae07da3e73b4c7f5f57509e9037
TanThanh-CityBoi <tanthanhe@gmail.com>
2024-03-23 09:01:24 +0700

    .

--- main/preload.ts
+++ main/preload.ts
@@ -4,6 +4,14 @@ const handler = {
     send(channel: string, value: unknown) {
         ipcRenderer.send(channel, value);
     },
+    sendSync(channel: string, value: unknown) {
+        // block the renderer process until a reply is received.
+        return ipcRenderer.sendSync(channel, value);
+    },
+    invoke(channel: string, value: unknown) {
+        //
+        return ipcRenderer.invoke(channel, value);
+    },
     on(channel: string, callback: (...args: unknown[]) => void) {
         const subscription = (_event: IpcRendererEvent, ...args: unknown[]) => callback(...args);
         ipcRenderer.on(channel, subscription);

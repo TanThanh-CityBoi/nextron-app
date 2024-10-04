ab4be97eaaf5e922cf197b7eb87178f5a0504d8f
TanThanhCityboi <tanthanhe@gmail.com>
2024-10-04 10:31:52 +0700

    feat: auto update

--- main/background.ts
+++ main/background.ts
@@ -1,15 +1,18 @@
 import { app } from 'electron';
+import { autoUpdater } from 'electron-updater';
 
-import { appEventHandler, createWindow, setAppPath, appSchedule } from '@/main/helpers';
+import { createWindow, setAppPath } from '@/main/helpers';
 import { ENV_CONFIG, WINDOW_CONFIG } from '@/main/configs';
-import ipcEventHandler from '@/main/ipc';
-//
+
 import * as dotenv from 'dotenv';
 dotenv.config(ENV_CONFIG);
 //
 const isProd = process.env.NODE_ENV === 'production';
 setAppPath(isProd);
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+const FEED_URL =
+    'https://github.com/TanThanh-CityBoi/nextron-app/releases/download/v1.0.0/nextron-app_1.0.0_amd64.deb';
 
 // create app
 (async () => {
@@ -21,11 +24,18 @@ process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
     } else {
         const port = process.argv[2];
         await mainWindow.loadURL(`http://localhost:${port}/sleep`);
-        mainWindow.webContents.openDevTools();
+        // mainWindow.webContents.openDevTools();
     }
-    // app schedule
-    appSchedule(mainWindow);
+
+    autoUpdater.setFeedURL(FEED_URL);
+    autoUpdater.checkForUpdatesAndNotify();
+
+    autoUpdater.on('update-available', () => {
+        console.log('Update available');
+    });
+
+    autoUpdater.on('update-downloaded', () => {
+        autoUpdater.quitAndInstall();
+    });
 })();
 
-appEventHandler();
-ipcEventHandler();
